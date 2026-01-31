5ca7afb5a56860133d36c877ebd237af2522267f
Musthak <musthakcool55@gmail.com>
2026-01-31 20:46:02 +0530

    personal blog api

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .forms import UserCreationForm, UserChangeForm
+from .models import User
 
-# Register your models here.
+class UserAdmin(BaseUserAdmin):
+    add_form = UserCreationForm
+    form = UserChangeForm
+    model = User
+
+    list_display = ["email", "username", "first_name", "last_name", "is_staff"]
+
+    fieldsets = BaseUserAdmin.fieldsets + (
+        (None, {
+            "fields": (
+                "bio", "profile_image"
+            ),
+        }),
+    )
+    
+    add_fieldsets = BaseUserAdmin.add_fieldsets + (
+        (None, {
+            "fields": (
+                "bio", "profile_image"
+            ),
+        }),
+    )
+
+admin.site.register(User, UserAdmin)
--- accounts/forms.py
+++ accounts/forms.py
@@ -1,7 +1,12 @@
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.forms import UserCreationForm as BaseUserCreationForm, UserChangeForm as BaseUserChangeForm
 from .models import User
 
-class UserCreationForm(UserCreationForm):
-    class Meta(UserCreationForm):
+class UserCreationForm(BaseUserCreationForm):
+    class Meta:
         model = User
-        fileds = ("first_name", "last_name", "username", "email")
+        fields = ("first_name", "last_name", "username", "email", "bio", "profile_image")
+
+class UserChangeForm(BaseUserChangeForm):
+    class Meta:
+        model = User
+        fields = ("first_name", "last_name", "username", "email", "bio", "profile_image")
