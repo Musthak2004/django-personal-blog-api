6650f388e0d388c6a191e5c617f3176b9d62bd2c
Musthak <musthakcool55@gmail.com>
2026-02-02 13:57:19 +0530

    personal blog api

--- blogs/models.py
+++ blogs/models.py
@@ -1,1 +1,20 @@
 from django.db import models
+from django.urls import reverse
+from django.conf import settings
+
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="posts")
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True)
+    content = models.TextField()
+    cover_image = models.ImageField(upload_to="posts/", blank=True, null=True)
+    status = models.CharField(max_length=20, choices=[("draft", "Draft"), ("published", "Published"), ("private", "Private")], default="draft")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+    
+    def get_absolute_url(self):
+        return reverse("post_detail", kwargs={"pk": self.pk})
+    
