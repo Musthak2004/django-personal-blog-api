b6807aed5a8f0b44e3e8b7d2e27d9bf872336b57
Musthak <musthakcool55@gmail.com>
2026-02-02 20:05:12 +0530

    personal blog api

--- blogs/views.py
+++ blogs/views.py
@@ -2,6 +2,7 @@ from django.views.generic import ListView, DetailView
 from django.views.generic.edit import UpdateView, DeleteView, CreateView
 from .models import Post
 from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 class PostListView(ListView):
     model = Post
@@ -10,15 +11,23 @@ class PostListView(ListView):
 class PostDetailView(DetailView):
     model = Post
     template_name = "post_detail.html"
-class PostUpdateView(UpdateView):
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
     fields = ("title", "content", "cover_image", "status")
     template_name = "post_update.html"
-class PostDeleteView(DeleteView):
+
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Post
     success_url = reverse_lazy("post_list")
     template_name = "post_delete.html"
-class PostCreateView(CreateView):
+
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
+class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
     template_name = "post_create.html"
     fields = ("title", "content", "cover_image", "status")
