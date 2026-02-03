d71662659f5db002974435f6287781eafb1cc54f
Musthak <musthakcool55@gmail.com>
2026-02-03 17:53:36 +0530

    comment model, view and templates created successfully

--- blogs/views.py
+++ blogs/views.py
@@ -1,32 +1,43 @@
-from django.views.generic import ListView, DetailView
-from django.views.generic.edit import UpdateView, DeleteView, CreateView
-from .models import Post
-from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, UpdateView, DeleteView, CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.urls import reverse_lazy
+from django.shortcuts import redirect
+from .models import Post
+from .forms import CommentForm
+
 
 class PostListView(ListView):
     model = Post
     template_name = "post_list.html"
     ordering = ["-updated_at"]
-class PostDetailView(DetailView):
+
+
+class PostDetailView(LoginRequiredMixin, DetailView):
     model = Post
     template_name = "post_detail.html"
-class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
-    model = Post
-    fields = ("title", "content", "cover_image", "status")
-    template_name = "post_update.html"
 
-    def test_func(self):
-        obj = self.get_object()
-        return obj.author == self.request.user
-class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
-    model = Post
-    success_url = reverse_lazy("post_list")
-    template_name = "post_delete.html"
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["form"] = CommentForm()
+        context["comments"] = self.object.comments.all()  # ✅ use comments not comment_set
+        return context
 
-    def test_func(self):
-        obj = self.get_object()
-        return obj.author == self.request.user
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        form = CommentForm(request.POST)
+
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = self.object
+            comment.author = request.user
+            comment.save()
+            return redirect("post_detail", pk=self.object.pk)
+
+        context = self.get_context_data()
+        context["form"] = form
+        return self.render_to_response(context)
+
+
 class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
     template_name = "post_create.html"
@@ -35,3 +46,21 @@ class PostCreateView(LoginRequiredMixin, CreateView):
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ("title", "content", "cover_image", "status")
+    template_name = "post_update.html"
+
+    def test_func(self):
+        return self.get_object().author == self.request.user
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = reverse_lazy("post_list")
+    template_name = "post_delete.html"
+
+    def test_func(self):
+        return self.get_object().author == self.request.user
